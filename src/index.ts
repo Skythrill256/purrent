9a35a641dcadb4142b0eaf2aff1e9629207d45f6
Skythrill256 <anjishnuganguly773@gmail.com>
2024-05-11 14:52:06 +0530

    Final commit

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,29 @@
-'use strict';
+type QueueKey = symbol
 
-type QueueKey = symbol;
+const KEY: QueueKey = Symbol('purrent:queue')
 
-const KEY: QueueKey = Symbol('purrent:queue');
+const DEFAULT_WHEN: (...args: any[]) => boolean = () => true
 
-const DEFAULT_WHEN: (...args: any[]) => boolean = () => true;
-
-const NEW_PROMISE = <T>(handler: (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void): Promise<T> => new Promise<T>(handler);
-export { KEY };
+const NEW_PROMISE = <T>(handler: (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void): Promise<T> => new Promise<T>(handler)
+export { KEY }
 
 export type ConcurrencyOptions<T> = {
-  concurrency: number;
-  global?: boolean;
-  when?: (...args: any[]) => boolean;
-  promise?: <T>(handler: (resolve: (value?: T | PromiseLike<T> | undefined) => void, reject: (reason?: any) => void) => void) => Promise<T>;
-  key?: QueueKey;
-};
+  concurrency: number
+  global?: boolean
+  when?: (...args: any[]) => boolean
+  promise?: <T>(handler: (resolve: (value?: T | PromiseLike<T> | undefined) => void, reject: (reason?: any) => void) => void) => Promise<T>
+  key?: QueueKey
+}
 
-export type LimiterFunction<T> = (fn: (...args: any[]) => Promise<T>) => (...args: any[]) => Promise<T>;
+export type LimiterFunction<T> = (fn: (...args: any[]) => Promise<T>) => (...args: any[]) => Promise<T>
 
 export const concurrency = <T>(options: ConcurrencyOptions<T>): LimiterFunction<T> => {
   if (typeof options === 'number') {
     options = {
       concurrency: options
-    } as ConcurrencyOptions<T>;
+    } as ConcurrencyOptions<T>
   } else if (Object(options) !== options) {
-    throw new TypeError('options must be an object or a number');
+    throw new TypeError('options must be an object or a number')
   }
 
   const {
@@ -34,59 +32,59 @@ export const concurrency = <T>(options: ConcurrencyOptions<T>): LimiterFunction<
     when = DEFAULT_WHEN,
     promise: promiseFactory = NEW_PROMISE,
     key = KEY
-  } = options;
+  } = options
 
   if (typeof concurrency !== 'number' || concurrency < 1) {
-    throw new TypeError('concurrency must be a number from 1 and up');
+    throw new TypeError('concurrency must be a number from 1 and up')
   }
 
   const limiter: LimiterFunction<T> = fn => {
     function limited(this: any, ...args: any[]): Promise<any> {
       if (!when.apply(this, args)) {
         // Should not be limited
-        return fn.apply(this, args) as Promise<any>;
+        return fn.apply(this, args) as Promise<any>
       }
 
-      const host: any = !!this && !useGlobalHost ? this : limiter;
+      const host: any = !!this && !useGlobalHost ? this : limiter
       const info = host[key] || (host[key] = {
         size: 0,
         queue: []
-      });
+      })
 
-      const { queue } = info;
+      const { queue } = info
 
       const next = () => {
-        info.size--;
+        info.size--
         if (queue.length > 0) {
-          queue.shift()!();
+          queue.shift()!()
         }
-      };
+      }
 
       return promiseFactory((resolve, reject) => {
         const run = () => {
-          info.size++;
+          info.size++
           fn.apply(this, args).then(
             value => {
-              resolve(value);
-              next();
+              resolve(value)
+              next()
             },
             err => {
-              reject(err);
-              next();
+              reject(err)
+              next()
             }
-          );
-        };
+          )
+        }
 
         if (info.size < concurrency) {
-          run();
+          run()
         } else {
-          queue.push(run);
+          queue.push(run)
         }
-      });
+      })
     }
 
-    return limited;
-  };
+    return limited
+  }
 
-  return limiter;
-};
+  return limiter
+}
