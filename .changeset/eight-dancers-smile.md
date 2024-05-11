4d14157553bec3436f3a4d9220afd09f55cda986
Skythrill256 <anjishnuganguly773@gmail.com>
2024-05-11 14:27:36 +0530

    First Commit

---
+++ .changeset/eight-dancers-smile.md
@@ -0,0 +1,5 @@
+---
+"purrent": patch
+---
+
+New version
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+const add = () =>{
+  var a:number = 10
+  var b:number = 20
+  var c:number = a + b
+  console.log(c)
+}
+
+export default add
